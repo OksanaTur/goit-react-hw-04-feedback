59d9e7a5d3ae2970e117b5b469479f4a1c4ed9a9
OksanaTur <ksyturkevych@gmail.com>
2023-03-10 13:30:15 +0400

    add files

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,22 @@
+import propTypes from 'prop-types';
+import { Item, List } from './Statistics.styled';
+
+export const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
+    <List>
+    <Item>Good: {good}</Item> 
+    <Item>Neutral: {neutral}</Item>
+    <Item>Bad: {bad}</Item>
+    <Item>Total: {total}</Item>
+    <Item>
+      Positive feedback: {positivePercentage} %
+    </Item>
+ </List>
+);
+
+Statistics.propTypes = {
+  good: propTypes.number.isRequired,
+  neutral: propTypes.number.isRequired,
+  bad: propTypes.number.isRequired,
+  total: propTypes.number.isRequired,
+  positivePercentage: propTypes.number.isRequired,
+};
---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+   display: flex;
+   flex-direction: column;
+   list-style: none;
+`;
+
+export const Item = styled.li`
+display: flex;
+align-items: left;
+font-size: 26px;
+padding: 8px;
+`;
